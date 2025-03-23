8f3f5715fa011b265045049c07ed1f429d12712c
ktang <jay.ang920914@gmail.com>
2025-03-23 16:55:38 +0800

    f

--- mobile/app/(auth)/_layout.jsx
+++
@@ -1,8 +0,0 @@
-import {Stack} from 'expo-router'
-import React from 'react'
-
-export default function _layout() {
-  return <Stack screenOptions={{headerShown:false}}>
-    <Stack.Screen name="Signin" options={{title:'Sign In'}}/>
-  </Stack>
-}
