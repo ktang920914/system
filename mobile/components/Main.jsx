f025b917495f972d45966c2c0c20a593ccff17d8
ktang <jay.ang920914@gmail.com>
2025-03-23 10:59:38 +0800

    fixed

--- mobile/components/Main.jsx
+++ mobile/components/Main.jsx
@@ -10,7 +10,7 @@ export default function main() {
       <Text className="text-gray-500 text-6xl font-extrabold mt-2">System</Text>
 
       <Link  className='mt-8 bg-slate-500 py-4 px-12 rounded-full text-white font-bold text-3xl' href='/(auth)/Signin'>
-        Sign In
+        JOIN
       </Link>
     </View>
   )
