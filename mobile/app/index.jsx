895fc2a1e6383de7f0b4fed543edb66acf7e0dc7
ktang <jay.ang920914@gmail.com>
2025-03-23 17:12:33 +0800

    f

---
+++ mobile/app/index.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import './global.css'
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
