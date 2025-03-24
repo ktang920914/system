17e614b6246ed7d9dd5cf7d0d2ad828e36f07882
ktang <jay.ang920914@gmail.com>
2025-03-25 07:50:19 +0800

    fixed

--- mobile/app/(tab)/_layout.jsx
+++ mobile/app/(tab)/_layout.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Tabs } from 'expo-router'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function _layout() {
   return (
@@ -12,6 +13,10 @@ export default function _layout() {
           tabBarIcon: ({color, size}) => <MaterialIcons name="table-bar" size={size} color={color} />
         }} 
       />
+      <Tabs.Screen name='Bill'
+      options={{
+        tabBarIcon: ({color, size}) => <Ionicons name="receipt-outline" size={size} color={color} />
+      }}  />
       <Tabs.Screen name='Profile'
       options={{
         tabBarIcon: ({color, size}) => <AntDesign name="user" size={size} color={color} />
