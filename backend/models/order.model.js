fc497bb1de4b96e8f65a8c604462b03be13dbb02
ktang <jay.ang920914@gmail.com>
2025-03-25 09:05:38 +0800

    f

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-    
+    table:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Table',
+    },
+    ordernumber:{
+        type:String,
+        required:true,
+        unique:true
+    }
 },{timestamps:true})
 
 const Order = mongoose.model('Order', orderSchema)
