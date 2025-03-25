420738b4c53f49d9bcc260491ba8c89e1e962bf1
ktang <jay.ang920914@gmail.com>
2025-03-25 16:24:25 +0800

    fixed

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -4,12 +4,46 @@ const orderSchema = new mongoose.Schema({
     table:{
         type:mongoose.Schema.Types.ObjectId,
         ref:'Table',
+        required:true,
     },
     ordernumber:{
         type:String,
         required:true,
         unique:true,
     },
+    orderitems:[{
+        orderproductname:{
+            type:String,
+            required:true,
+        },
+        orderproductquantity:{
+            type:Number,
+            required:true,
+        },
+        orderproductprice:{
+            type:Number,
+            required:true,
+        },
+    }],
+    ordercomboitem:[{
+        comboproductitem:{
+            type:String,
+            required:true,
+        },
+        comboproductquantity:{
+            type:Number,
+            required:true,
+        },
+    }],
+    ordertax:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required:true,
+    },
+    servicetax:{
+        type:Number,
+        default:8
+    }
 },{timestamps:true})
 
 const Order = mongoose.model('Order', orderSchema)
