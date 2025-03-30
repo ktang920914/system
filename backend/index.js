60769927b4eeb8fc1683e555cb80a351f1e84778
ktang <jay.ang920914@gmail.com>
2025-03-30 10:10:30 +0800

    f

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use(express.json());
 app.use(cookieParser());
 
