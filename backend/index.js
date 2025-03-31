25a35b3da12723c6f31d8ed6dc6f78a64814a1ef
ktang <jay.ang920914@gmail.com>
2025-03-31 10:28:28 +0800

    f

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ import orderRoute from './routes/order.route.js'
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
